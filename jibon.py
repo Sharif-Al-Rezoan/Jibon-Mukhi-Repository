5cef00b1ccdc2aad1b3006829a97ca407ef51251
Sharif-Al-Rezoan <131853680+Sharif-Al-Rezoan@users.noreply.github.com>
2023-12-01 10:56:45 +0600

    Update jibon.py

--- jibon.py
+++ jibon.py
@@ -1,3 +1,28 @@
 var = "Jibon e bedona "
 for i in range(10):
    print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   var = "Jibon e bedona "
+for i in range(10):
+   print(var)
+   
